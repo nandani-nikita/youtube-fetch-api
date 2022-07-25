6e412a14808906970f5cf81aa15cd9191dd4ad1d
Nikita <nikitann2501@gmail.com>
2022-07-25 22:53:36 +0530

    Docker File Added

--- server/Services/cronService.js
+++ server/Services/cronService.js
@@ -2,7 +2,7 @@ var cron = require('node-cron');
 const { videoMining } = require('../Services/youtubeService');
 const dbServices = require('./dbService');
 
-cron.schedule('* */2 * * * *', async () => {
+cron.schedule('*/2 * * * *', async () => {
     console.log('running a task every two minutes');
 
     // // fetching videos every two minutes
--- server/models/db.js
+++ server/models/db.js
@@ -4,8 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config({ path: './.env' });
 
 // mongo db connection
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlparser: true }, (err) => {
-mongoose.connect('mongodb://127.0.0.1:27017/userAndTeam', { useNewUrlparser: true }, (err) => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlparser: true }, (err) => {
+// mongoose.connect('mongodb://127.0.0.1:27017/userAndTeam', { useNewUrlparser: true }, (err) => {
     if (!err) {
         console.log('Mongo Connection Successful.');
     } else {
