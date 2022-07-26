68a91688fe380c5dcea2d0f546b2af596685f611
Nikita <nikitann2501@gmail.com>
2022-07-26 08:56:11 +0530

    Backend-complete

--- server/Services/cronService.js
+++ server/Services/cronService.js
@@ -5,16 +5,16 @@ const dbServices = require('./dbService');
 cron.schedule('*/2 * * * *', async () => {
     console.log('running a task every two minutes');
 
-    // // fetching videos every two minutes
-    // const response = await videoMining();
-    // console.log(response.length);
+    // fetching videos every two minutes
+    const response = await videoMining();
+    console.log(response.length);
 
-    // // storing the fetched videos in db
-    // response.forEach(element => {
-    //     try {
-    //         dbServices.insertService(element)
-    //     } catch (error) {
-    //      console.log(error);   
-    //     }
-    // });
+    // storing the fetched videos in db
+    response.forEach(element => {
+        try {
+            dbServices.insertService(element)
+        } catch (error) {
+         console.log(error);   
+        }
+    });
 });
